25c57d89c90b7e9ce901ef19c241e67f31ab4ec0
Jon Rea <jonarea@gmail.com>
2020-12-01 11:38:38 -0500

    update mongoose connection

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 var mongoose = require("mongoose");
 var mongoDB = process.env.MONGODB_URI;
 
-mongoose.connect(mongoDB, { useMongoClient: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error"));
 
